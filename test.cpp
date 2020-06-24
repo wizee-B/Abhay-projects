d59eeced31eaaf7219a1d38a662f5055c69bc019
Abhay Gupta <nextresp@gmail.com>
2020-06-24 06:47:04 +0530

    wave matrix printing

--- test.cpp
+++ test.cpp
@@ -2,7 +2,29 @@
 using namespace std;
 
 int main(){
-    cout<< "Abhay";
+    int A[100][100];
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)cin>>A[i][j];
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)cout<<A[i][j]<<" ";cout<<endl;
+    }
+    cout<<endl;
+     for(int i=0;i<m;){
+        for(int j=0;j<n;j++){
+            cout<<A[j][i]<<" ";
+            }i++;
+        if(i<n){
+            for(int j=n-1;j>=0;j--){
+            cout<<A[j][i]<<" ";
+            }
+            i++;
+        }
+    }
+
     return 0;
 }
 
